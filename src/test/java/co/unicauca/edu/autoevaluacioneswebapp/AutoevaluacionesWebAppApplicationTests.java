40516f6990a488da42a57b7d65a3815c64cd37d9
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-11-04 01:36:16 -0500

    Added base project

---
+++ src/test/java/co/unicauca/edu/autoevaluacioneswebapp/AutoevaluacionesWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.unicauca.edu.autoevaluacioneswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoevaluacionesWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
