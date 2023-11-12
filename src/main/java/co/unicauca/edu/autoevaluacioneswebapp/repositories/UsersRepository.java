be0dbc4801c5570640ca42e69b61b0a8129bf4a1
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-11-11 21:25:42 -0500

    removed some things

--- src/main/java/co/unicauca/edu/autoevaluacioneswebapp/repositories/UsersRepository.java
+++ src/main/java/co/unicauca/edu/autoevaluacioneswebapp/repositories/UsersRepository.java
@@ -2,6 +2,7 @@ package co.unicauca.edu.autoevaluacioneswebapp.repositories;
 
 import co.unicauca.edu.autoevaluacioneswebapp.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 public interface UsersRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String Email);
 }
--- src/main/java/co/unicauca/edu/autoevaluacioneswebapp/services/UserService.java
+++ src/main/java/co/unicauca/edu/autoevaluacioneswebapp/services/UserService.java
@@ -1,11 +1,21 @@
 package co.unicauca.edu.autoevaluacioneswebapp.services;
 
 import co.unicauca.edu.autoevaluacioneswebapp.entities.UserEntity;
+import co.unicauca.edu.autoevaluacioneswebapp.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class UserService {
+    UsersRepository usersRepository;
+
+    @Autowired
+    public UserService(UsersRepository usersRepository){
+        this.usersRepository = usersRepository;
+    }
     public Optional<UserEntity> getUser(String email){
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        return  this.usersRepository.findByEmail(email);
     }
 }
