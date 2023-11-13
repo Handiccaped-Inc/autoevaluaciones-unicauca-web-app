796f529c9ef252fbd59ac38facee06f62af22134
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-11-12 23:45:01 -0500

    Added the logout url

--- src/main/java/co/unicauca/edu/autoevaluacioneswebapp/configuration/SecurityConfiguration.java
+++ src/main/java/co/unicauca/edu/autoevaluacioneswebapp/configuration/SecurityConfiguration.java
@@ -33,6 +33,10 @@ public class SecurityConfiguration {
                             flg.successHandler(successHandler());
                         }
                 )
+                .logout((lgo) -> lgo
+                        .permitAll()
+                        .logoutSuccessUrl("/login")
+                )
                 .sessionManagement((ssmg) -> {
                             ssmg.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                                     .invalidSessionUrl("/login")
