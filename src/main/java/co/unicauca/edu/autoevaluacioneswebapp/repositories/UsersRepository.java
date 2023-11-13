e0d180166bc39520230f7a7e9ca4bd66ba7c9f79
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-11-13 01:27:59 -0500

    the first form create a proffesor

--- src/main/java/co/unicauca/edu/autoevaluacioneswebapp/repositories/UsersRepository.java
+++ src/main/java/co/unicauca/edu/autoevaluacioneswebapp/repositories/UsersRepository.java
@@ -2,7 +2,10 @@ package co.unicauca.edu.autoevaluacioneswebapp.repositories;
 
 import co.unicauca.edu.autoevaluacioneswebapp.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 public interface UsersRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String Email);
+    List<UserEntity> findAllByprofessorTypes(String professorType);
 }
--- src/main/java/co/unicauca/edu/autoevaluacioneswebapp/services/UserService.java
+++ src/main/java/co/unicauca/edu/autoevaluacioneswebapp/services/UserService.java
@@ -6,16 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
 @Service
-public class UserService implements IUserService{
+public class UserService implements IUserService {
     UsersRepository usersRepository;
 
     @Autowired
-    public UserService(UsersRepository usersRepository){
+    public UserService(UsersRepository usersRepository) {
         this.usersRepository = usersRepository;
     }
-   @Override
-    public Optional<UserEntity> findByEmail(String email){
-        return  this.usersRepository.findByEmail(email);
+
+    @Override
+    public Optional<UserEntity> findByEmail(String email) {
+        return this.usersRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserEntity save(UserEntity user) {
+        return this.usersRepository.save(user);
+    }
+
+    @Override
+    public UserEntity update(UserEntity user) {
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        usersRepository.deleteById(id);
     }
 }
