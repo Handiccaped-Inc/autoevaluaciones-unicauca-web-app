195f2f52c799a32ed7bdbc1c712d88b49f69c210
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-11-13 18:00:03 -0500

    Added some example methods

--- src/main/java/co/unicauca/edu/autoevaluacioneswebapp/repositories/UserRoleRepository.java
+++ src/main/java/co/unicauca/edu/autoevaluacioneswebapp/repositories/UserRoleRepository.java
@@ -1,10 +1,15 @@
 package co.unicauca.edu.autoevaluacioneswebapp.repositories;
 
+import co.unicauca.edu.autoevaluacioneswebapp.model.ERole;
 import co.unicauca.edu.autoevaluacioneswebapp.model.UserEntity;
 import co.unicauca.edu.autoevaluacioneswebapp.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-    public UserRole findByUser(UserEntity userEntity);
+    public Optional<UserRole>findByUser(UserEntity userEntity);
+    public List<UserRole> findByRoleName(ERole role_name);
+
 }
-    
