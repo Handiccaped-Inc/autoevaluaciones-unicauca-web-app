40f0c1ad39d39b5e18d18c9f210fb06eb68ddc89
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-11-16 19:39:28 -0500

    Just one step left

--- src/main/java/co/unicauca/edu/autoevaluacioneswebapp/facades/AutoevaluationFacade.java
+++ src/main/java/co/unicauca/edu/autoevaluacioneswebapp/facades/AutoevaluationFacade.java
@@ -1,4 +1,22 @@
 package co.unicauca.edu.autoevaluacioneswebapp.facades;
 
+
+import co.unicauca.edu.autoevaluacioneswebapp.services.LabourService;
+import co.unicauca.edu.autoevaluacioneswebapp.services.UserRoleService;
+import co.unicauca.edu.autoevaluacioneswebapp.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AutoevaluationFacade {
+    private LabourService labourService;
+
+    private UserRoleService userRoleService;
+
+    private UserService userService;
+
+    private AutoevaluationFacade(LabourService labourService, UserRoleService userRoleService, UserService userService){
+        this.labourService = labourService;
+        this.userRoleService = userRoleService;
+        this.userService = userService;
+    }
 }
