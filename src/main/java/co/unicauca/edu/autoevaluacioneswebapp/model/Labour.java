040b58bc0403d352455a1a4c1683dc7a9575ba4e
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-11-21 19:47:48 -0500

    added builder and set true as default for Labour

--- src/main/java/co/unicauca/edu/autoevaluacioneswebapp/model/Labour.java
+++ src/main/java/co/unicauca/edu/autoevaluacioneswebapp/model/Labour.java
@@ -14,6 +14,7 @@ import org.hibernate.annotations.Cascade;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,6 +24,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Labour {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,6 +38,6 @@ public class Labour {
     private LabourType type;
 
     private int assignedHours;
-
+    @Builder.Default
     private boolean active = true;
 }
